780e2bca9deb4bab377b725df1501ae8c9e6119b
EddyVerbruggen <eddyverbruggen@gmail.com>
2015-09-10 11:36:40 +0200

    #131 Incompatible with iOS9 Public Beta 1#127 iOS 9 without local webserver. Found an incompatibility when building with XCode 7 and running on iOS 8.4.1.

--- src/ios/MyMainViewController.m
+++ src/ios/MyMainViewController.m
@@ -219,17 +219,21 @@
     self.alreadyLoaded = true;
     // /////////////////
     [CDVUserAgentUtil acquireLock:^(NSInteger lockToken) {
-        _userAgentLockToken = lockToken;
-        [CDVUserAgentUtil setUserAgent:self.userAgent lockToken:lockToken];
+      _userAgentLockToken = lockToken;
+      [CDVUserAgentUtil setUserAgent:self.userAgent lockToken:lockToken];
 
     // This is only for iOS 9 SDK
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 90000
-    [self.wkWebView loadFileURL:URL allowingReadAccessToURL:URL];
-#else
+      if (IsAtLeastiOSVersion(@"9.0")) {
+        [self.wkWebView loadFileURL:URL allowingReadAccessToURL:URL];
+      } else {
         NSURLRequest* appReq = [NSURLRequest requestWithURL:URL cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:20.0];
-            [self.wkWebView loadRequest:appReq];
+        [self.wkWebView loadRequest:appReq];
+      }
+#else
+      NSURLRequest* appReq = [NSURLRequest requestWithURL:URL cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:20.0];
+      [self.wkWebView loadRequest:appReq];
 #endif
-
     }];
 }
 
